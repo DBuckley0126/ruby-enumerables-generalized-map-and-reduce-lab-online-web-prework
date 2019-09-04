6ea39375a036f78d0d9347eb52a43bcd257381f0
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-04 18:07:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,6 +29,7 @@ def reduce(source_array, starting_point=0)
   i = 0
   while i < source_array.length do
     answer = yield(source_array[i], total)
+    answer = total
     i += 1
   end
 return total
