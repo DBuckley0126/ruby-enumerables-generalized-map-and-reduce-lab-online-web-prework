b081d20ef51086ae70098b5e90a37f7dca9bf64f
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-04 17:24:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,5 +13,15 @@ end
 
 #reduce
 
+def reduce_to_total(source_array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < source_array.length do
+    new += source_array[i]
+    i += 1
+  end
+  return new
+end
 
-def rescue()
+def rescue(source_array, starting_point=0)
+  
